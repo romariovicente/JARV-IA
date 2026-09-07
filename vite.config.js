4793a4572f99b0f7946e688f7b1e254569912c95
Romario Vicente <juuzousuzuyabdt@gmail.com>
2026-09-07 00:35:00 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3001',
-        changeOrigin: true
-      }
-    }
-  }
+  base: '/JARV-IA/',
 })
